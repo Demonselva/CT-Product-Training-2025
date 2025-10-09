fd628622ab7ed06bdd3fd85466a59ddc60960154
Selva Kumar <149900433+Demonselva@users.noreply.github.com>
2025-10-09 15:04:15 +0530

    new Day

---
+++ DAY13/DAY13/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(Math.Max(5, 10));
+Console.WriteLine(Math.Min(5, 10));
+Console.WriteLine(Math.Round(9.9));
+Console.WriteLine(Math.Round(9.5));
+Console.WriteLine(Math.Round(9.4));
+Console.WriteLine(Math.Sqrt(16));
+Console.WriteLine(Math.Abs(-9));
+Console.WriteLine(Math.Pow(4,2));
+
+
+int a = 5;
+a++;
+int  b = 6;
+Console.WriteLine();
+Console.WriteLine(a);
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a % b);
+Console.WriteLine(a / b);
+Console.WriteLine(a == b);
+Console.WriteLine(a > b);
+Console.WriteLine(++a);
+Console.WriteLine(a);
+++a;
+Console.WriteLine(a);
+
+
---
+++ DAY14/DAY14/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+Random random = new Random();
+
+Console.Write("Enter the number of guesses : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int i = 0;
+
+while (i < a)
+{
+    Console.Write($"{i+1}) guess the number 1 to 100 : ");
+    int guess = Convert.ToInt32(Console.ReadLine());
+    int target=random.Next(1,101);
+    if (guess != target)
+    {
+        
+        Console.WriteLine($"Good Luck Next Time!! \n Random no:{target} \n Guess no={guess} ");
+        if (guess < target)
+        {
+            Console.WriteLine("Too Low");
+        }
+        else
+        {
+            Console.WriteLine("Too High");
+        }
+            i++;
+  
+    }
+    else
+    {
+        Console.WriteLine($"Your lucky!! \n Random no:{target} \n Guess no={guess} ");
+        break;
+    }
+}
